9f96693b691bf9c62a8281fec9ece132a2b4eaf9
xingkui wang <bit3725@gmail.com>
2019-07-10 15:35:33 +0800

    can show action data in react native view

--- RNActionExtension/extension.js
+++ RNActionExtension/extension.js
@@ -6,16 +6,38 @@ import {
   Text,
   TouchableOpacity,
 } from 'react-native';
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {name as appName} from './app.json';
 
+
 const Share = () => {
+  const {done, getData} = NativeModules.ActionExtension;
+  const [data, updateData] = useState();
+  useEffect(() => {
+    const getDataFromExtension = async () => {
+      try {
+        const extensionData = await getData();
+        updateData(extensionData);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    getDataFromExtension();
+  }, []);
+
   return (
     <SafeAreaView style={{flex: 1}}>
       <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
         <Text>Action Extension</Text>
+        {data && (
+          <View>
+            <Text>{data.type}</Text>
+            <Text>{data.value}</Text>
+          </View>
+          )
+        }
         <TouchableOpacity onPress={() => {
-          NativeModules.ActionExtension.done();
+          done();
         }}>
           <Text style={{color: 'blue'}}>Close</Text>
         </TouchableOpacity>
